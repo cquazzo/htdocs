f51ee9d638b0655afa948feb10cbac6fd462d880
Chris Quazzo <chrisquazzo@gmail.com>
2013-06-08 12:36:15 -0500

    Test Commit

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <!--<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>-->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.min.js"></script> 
 
   </body>
 </html>
