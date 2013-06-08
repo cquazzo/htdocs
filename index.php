deec95d9d08b6c2f62f2e4e2f45fa2ddaa3fa966
Marcos Alcozer <mealcozer@gmail.com>
2013-06-08 15:14:41 -0500

    Added script tag to include JQuery
    Check out line 64, we forgot to include JQuery. Such a small thing held us up! As a reminder, JQuery is a JavaScript library that you can learn about at http://www.codecademy.com/tracks/jquery and http://jquery.com . The Bootstrap module modal utilizes JQuery. You will notice on line 64 I am utlizing a file that is stored on Google's servers, similar to what we did with the Bootstrap files.

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <!-- Le styles -->
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
     <style>
@@ -15,20 +13,12 @@
         padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
       }
     </style>
-    
-
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
       <script src="../assets/js/html5shiv.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
-   
-                                   
+    <![endif]-->                               
   </head>
-
   <body>
-
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
@@ -48,20 +38,11 @@
         </div>
       </div>
     </div>
-
-
-
     <div class="container">
-
       <h1>Bootstrap starter template.</h1>
       <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
-
-      
-  
-            <!-- Button to trigger modal -->
-      
-      <button type="button" data-toggle="modal" data-target="#myModal" class="btn">Launch modal</button>
-       
+      <!-- Button to trigger modal -->
+      <button data-toggle="modal" data-target="#myModal" class="btn">Launch modal</button>
       <!-- Modal -->
       <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-header">
@@ -76,17 +57,11 @@
           <button class="btn btn-primary">Save changes</button>
         </div>
       </div>
-
-
-    </div> 
-
-
-
+    </div>
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <!--<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>-->
-    <script src="js/bootstrap.min.js"></script> 
-
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
   </body>
 </html>
